28c4ed628f4c98c3ad5ddbb32acede67f467e560
Ajay Kumar <arvindkumar20102000@gmail.com>
2025-06-24 16:24:45 +0530

    frontend

--- vite.config.js
+++ vite.config.js
@@ -8,16 +8,21 @@
 
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react(),tailwindcss()],
+    plugins: [react(), tailwindcss()],
     preview: {
       host: '0.0.0.0',
       port: env.PORT ? parseInt(env.PORT) : 4173,
-      allowedHosts: ['dealfrontend-2.onrender.com'],
+      allowedHosts: [
+        'dealfrontend-2.onrender.com',
+        'dealfrontend-3.onrender.com', // ✅ Add this line
+      ],
     },
   };
 });
+
